a63d02d443cf9ffa986126fd320ba963a90afe60
Joan Soler <joansolerprova88@gmail.com>
2022-11-10 18:57:08 +0100

    surten les pomes un chic malament

--- SNAKE/MainWindow.xaml.cs
+++ SNAKE/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Snake2;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace SNAKE
 {
@@ -20,9 +22,62 @@ namespace SNAKE
     /// </summary>
     public partial class MainWindow : Window
     {
+        SnakeGame jocSerp;
+        DispatcherTimer timer = new DispatcherTimer();
         public MainWindow()
         {
+           
             InitializeComponent();
+            jocSerp = new SnakeGame();
+            jocSerp.ColocarPomes();
+            timer.Interval = TimeSpan.FromMilliseconds(500);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            int tamanyXCasella = (int)(canvas.ActualWidth / SnakeGame.X_SIZE);
+            int tamanyYCasella = (int)(canvas.ActualHeight / SnakeGame.Y_SIZE);
+
+            foreach (var poma in jocSerp.posicioPomes)
+            {
+                int tamanyXCasellap = (int)(canvas.ActualWidth / SnakeGame.X_SIZE);
+                int tamanyYCasellap = (int)(canvas.ActualHeight / SnakeGame.Y_SIZE);
+                Ellipse dibuixPoma = new Ellipse();
+                dibuixPoma.Fill = Brushes.Red;
+                dibuixPoma.Width = tamanyXCasellap;
+                dibuixPoma.Height = tamanyYCasellap;
+                Canvas.SetLeft(dibuixPoma, poma.X * tamanyXCasellap);
+                Canvas.SetTop(dibuixPoma, poma.Y * tamanyYCasellap);
+                canvas.Children.Add(dibuixPoma);
+            }
+            
+            Ellipse ellSerp = new Ellipse()
+            {
+                Fill = Brushes.Black,
+                Width = tamanyXCasella,
+                Height = tamanyYCasella
+            };
+            canvas.Children.Add(ellSerp);
+            Canvas.SetTop(ellSerp, jocSerp.CapSerp.Y * tamanyYCasella);
+            Canvas.SetLeft(ellSerp, jocSerp.CapSerp.X / tamanyXCasella);
+
+            jocSerp.MirarSiPerd();
+        }
+        private void canvas_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            jocSerp.moure();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            jocSerp.JocNou();
+        }
+
+        private void canvas_KeyDown(object sender, KeyEventArgs e)
+        {
+            jocSerp.moure();
         }
     }
 }
---
+++ SNAKE/SnakeGame.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Snake2
+{
+    public enum DireccioSnake
+    {
+        Dreta,
+        Esquerre,
+        Avall,
+        Amunt
+    }
+    class SnakeGame
+    {
+        public const int NPOMES = 5;
+        public const int X_SIZE = 10;
+        public const int Y_SIZE = 10;
+
+        public List<Point> posicioPomes = new List<Point>();
+        Random rand = new Random(); 
+        Point capSerp = new Point(0, 0);
+
+        DireccioSnake direccio = DireccioSnake.Avall;
+
+        public DireccioSnake Direccio { get => direccio; set => direccio = value; }
+        public Point CapSerp { get => capSerp; set => capSerp = value; }
+        List<Point> snake = new List<Point>();
+
+        internal void moure()
+        {
+            if (direccio == DireccioSnake.Dreta)
+            {
+                capSerp = snake[0];
+                Point nouCap = new Point(capSerp.X + X_SIZE, capSerp.Y);
+                snake.Insert(0, nouCap);
+                snake.RemoveAt(snake.Count - 1);
+            }
+            if (direccio == DireccioSnake.Esquerre)
+            {
+                Point capSerp = snake[0];
+                Point nouCap = new Point(capSerp.X - X_SIZE, capSerp.Y);
+                snake.Insert(0, nouCap);
+                snake.RemoveAt(snake.Count - 1);
+            }
+            if (direccio == DireccioSnake.Amunt)
+            {
+                Point capSerp = snake[0];
+                Point nouCap = new Point(capSerp.X, capSerp.Y - Y_SIZE);
+                snake.Insert(0, nouCap);
+                snake.RemoveAt(snake.Count - 1);
+            }
+            if (direccio == DireccioSnake.Avall)
+            {
+                Point capSerp = snake[0];
+                Point nouCap = new Point(capSerp.X, capSerp.Y + Y_SIZE);
+                snake.Insert(0, nouCap);
+                snake.RemoveAt(snake.Count - 1);
+            }
+        }
+        public void ColocarPomes()
+        {
+            for (int i = 0; i < NPOMES; i++)
+            {
+                posicioPomes.Add(new Point(rand.Next(0, X_SIZE), rand.Next(0, Y_SIZE)));
+            }
+        }
+
+        private void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (Keyboard.IsKeyDown(Key.Up))
+                direccio = DireccioSnake.Amunt;
+            if (Keyboard.IsKeyDown(Key.Down))
+                direccio = DireccioSnake.Avall;
+            if (Keyboard.IsKeyDown(Key.Left))
+                direccio = DireccioSnake.Esquerre;
+            if (Keyboard.IsKeyDown(Key.Right))
+                direccio = DireccioSnake.Dreta;
+        }
+        public void MirarSiPerd()
+        {
+
+            if (capSerp.Y > Y_SIZE || capSerp.Y < 0 || capSerp.X < 0 || capSerp.X > X_SIZE)
+            {
+                MessageBox.Show("Has perdut");
+            }
+        }
+        public void JocNou()
+        {
+            direccio = DireccioSnake.Dreta;
+
+            snake = new List<Point>();
+            snake.Add(new Point(X_SIZE * 10, Y_SIZE));
+            snake.Add(new Point(X_SIZE * 10, Y_SIZE * 2));
+            snake.Add(new Point(X_SIZE * 10, Y_SIZE * 3));
+            snake.Add(new Point(X_SIZE * 10, Y_SIZE * 4));
+        }
+    }
+}
